020b0de32d46024d706a6faca8689cba5cd70e19
yukihiro3710 <59110433+yukihiro3710@users.noreply.github.com>
2020-03-02 12:19:03 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,5 +4,12 @@ class Item < ApplicationRecord
   validates :place, presence: true
   validates :deliverydate, presence: true
   scope :name_like, -> name { where('name like ?', "%#{name}%") if name.present? }
+  def self.search(search)
+    if search
+      Item.where(['content LIKE ?', "%#{search}%"])
+    else
+      Item.all
+    end
+  end
 end
 
